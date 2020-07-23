c0b80561d8f7e4c215f1614751d9713f9785b4f4
saqlain-ahmed-123 <saqlainali1980@gmail.com>
2020-07-23 16:24:36 +0500

    first commit

--- assigment  chapter 39 to 67/chapter 52 to 57/app.js
+++ assigment  chapter 39 to 67/chapter 52 to 57/app.js
@@ -13,9 +13,20 @@ x.setAttribute("src", imgArray[i].src);
 x.setAttribute("width", "304");
 x.setAttribute("height", "228");
 x.setAttribute("alt", "alt");
-x.setAttribute("onClick", "getModel();");
+x.setAttribute("onClick", "getModel(this);");
+x.setAttribute("data-toggle", "modal");
+x.setAttribute("data-target", "#exampleModalCenter");
 document.body.appendChild(x);
 
+
+}
+
+function getModel(e)
+{
+    var getimg=document.getElementById('setImg');
+    getimg.setAttribute('src',e.src);
+    getimg.setAttribute('width','300px');
+    getimg.setAttribute('height','300px');
 }
 
 
